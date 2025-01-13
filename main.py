a215852d6d1cf09a38ff4451064db50569e0193b
Nicolas vauché <hello@nicolasvauche.net>
2025-01-13 14:00:10 +0100

    API Spaghetti is done

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ from typing import List
 
 app = FastAPI()
 
+
 class Task(BaseModel):
     id: int
     title: str
     done: bool = False
+
 
 tasks: List[Task] = [
     Task(id=1, title="Apprendre Python"),
     Task(id=2, title="Créer une API REST"),
 ]
 
+
 @app.get("/tasks", response_model=List[Task])
 def get_tasks():
     return tasks
+
 
 @app.post("/tasks", response_model=Task)
 def add_task(task: Task):
     tasks.append(task)
     return task
+
 
 @app.put("/tasks/{task_id}", response_model=Task)
 def update_task(task_id: int, updated_task: Task):
